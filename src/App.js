0a780f128e0a66b7378a4aaa00d03d7b7fb2d76b
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2019-10-29 07:35:00 -0500

    Add images

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+
+import './styles.css'
 
 function App() {
+  const [showImages, setShowImages] = useState(true)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <button onClick={() => setShowImages(show => !show)}>
+        {showImages ? 'hide' : 'show'} images
+      </button>
+      <div className="App">
+        {showImages &&
+          Array.from({ length: 12 }).map((_, id) => (
+            <SuspenseImage
+              key={id}
+              src={`images/${id}.jpeg`}
+              alt={`dog ${id}`}
+            />
+          ))}
+      </div>
+    </>
+  )
 }
 
-export default App;
+const SuspenseImage = ({ src, alt }) => {
+  // const [dataUrl, setDataUrl] = useState(null)
+  // useEffect(() => {
+  //   const img = new Image()
+  //   img.src = 'images/0.jpeg'
+  //   img.addEventListener('load', function() {
+  //     const imgCanvas = document.createElement('canvas')
+  //     const imgContext = imgCanvas.getContext('2d')
+
+  //     imgContext.drawImage(img, 0, 0)
+
+  //     setDataUrl(imgCanvas.toDataURL('image/jpeg'))
+  //   })
+  // }, [])
+
+  // if (!dataUrl) return null
+
+  return <img src={src} alt={alt} />
+}
+
+export default App
